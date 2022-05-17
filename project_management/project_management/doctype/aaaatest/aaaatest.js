74c8bd485480e1f5bbbd397ca57f91b62263a26e
ctqhbn <truongngoclinhtt12@gmail.com>
2022-05-17 16:48:42 +0700

    update

---
+++ project_management/project_management/doctype/aaaatest/aaaatest.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Me and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AAAATEST', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ project_management/project_management/doctype/aatesttest/test_aatesttest.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Me and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAATESTTEST(FrappeTestCase):
+	pass
